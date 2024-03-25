288a25822a2331fb3a70965bea1676f7b1218482
jakenotcakes <161679337+jakenotcakes@users.noreply.github.com>
2024-03-25 22:32:38 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const img = document.querySelector(".img");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+const secondQuestion = document.querySelectorAll(".question")[1];
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "I know you already know that i like you";
+  secondQuestion.innerHTML = "But Can i ask you if i can court you"; 
+  img.src =
+    "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/AskCrush1.png";
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
+
+let clickCount = 0;
+function handleClick() {
+    clickCount++; 
+    if (clickCount === 2) {
+        window.location.href = "https://jakenotcakes.github.io/Flower/"; 
+    }
+}
+
+yesBtn.addEventListener("click", handleClick);
